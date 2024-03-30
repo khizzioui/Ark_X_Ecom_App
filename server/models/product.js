849fa7a915330b6686660245ffd21338996bea47
yousra <d.1029.yousra@gmail.com>
2024-03-30 17:33:00 +0000

    testing the branch

--- server/models/product.js
+++ server/models/product.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const ProductSchema = new mongoose.Schema({
+    //product pic (ma3reftch chnu type)
     id: { type: Number, required: true},
     name: { type: String, required: true },
     description: { type: Text, required: true },
