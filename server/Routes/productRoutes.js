b0e3fd4d663d5f03f28f0d37f4af510d5084585b
yousra <d.1029.yousra@gmail.com>
2024-04-08 22:55:20 +0000

    fix product routes

--- server/Routes/productRoutes.js
+++ server/Routes/productRoutes.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
 
-router.get('/product', productController.getAllPost);
-router.get('/product/:id', productController.getPostById);
+router.get('/product', productController.getAllProducts);
+router.get('/product/:id', productController.getProductById);
 router.post('/product', productController.addProduct);
 router.put('/product/:id', productController.updateProduct);
 router.delete('/product/:id', productController.deleteProduct);
