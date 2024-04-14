15db781bcf8f95825d23eb8824f1319b17205414
¨khizzioui¨ <khiziouii@gmail.com>
2024-04-14 19:25:16 +0100

    update midellware and search service

--- server/Middlewares/searchMiddleware.js
+++ server/Middlewares/searchMiddleware.js
@@ -1,10 +1,18 @@
-const { check } = require('express-validator');
+const { check ,validationResult} = require('express-validator');
 
 
 
-async function searchMiddleware(){
-    check('q').notEmpty().withMessage('Field is required')
-}
+const searchMiddleware = [
+    check('q').notEmpty().withMessage('Field is required'),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        
+        next();
+    }
+];
 
 
 module.exports = {
--- server/services/searchService.js
+++ server/services/searchService.js
@@ -1,41 +1,87 @@
+require('dotenv').config();
 const Product = require('../Models/product');
+const ipipi = require('ipapi.co');
 
 
-
+const callback = function(loc){
+    console.log(loc)
+    return loc;
+}
 
 
 const searchService = {
     search : async (data) => {
-        const products = await Product.find({name:{$regex:search,$options:"i"}})
-                                        .skip(data.page*data.limit)
-                                        .limit(data.limit).aggregate([
-                                        {
-                                            $geonear:{
-                                                $near:{
-                                                    type:"point",
-                                                    coordinates:[
+        console.log(data.ip)
+        if (!data.location.lat || !data.location.lang){
+            const location = ipipi.location(callback,data.ip)
+            console.log(JSON.stringify(location));
+        }
+        const products = await Product.find({
+            title: {
+                $regex: data.search,
+                $options: "i"
+            }
+        }).exec();
+        
+        
+        const ProductIds = products.map(product => product._id);
+        
+        
+        const geospatialResults = await Product.aggregate([
+            {
+                $geoNear: {
+                    near: {
+                        type: "Point",
+                        coordinates: [
+                            31.633891, -8.032572
+                        ]
+                    },
+                    distanceField: "dist.calculated",
+                    maxDistance: Number(data.radius) * 1000, // Include maxDistance here
+                    spherical: true
+                }
+            },
+            {
+                $facet: {
+                    totalCount: [
+                        {
+                            $match: {
+                                _id: { $in: ProductIds } 
+                            }
+                        },
+                        {
+                            $count: "total"
+                        }
+                    ],
+                    paginatedResults: [
+                        {
+                            $match: {
+                                _id: { $in: ProductIds } 
+                            }
+                        },
+                        {
+                            $skip: data.page * data.limit
+                        },
+                        {
+                            $limit: data.limit
+                        }
+                    ]
+                }
+            }
+        ]).exec();
+        
 
-                                                    ]
-                                                },
-                                                distanceField:"distance",
-                                                maxDistance: data.rayon * 1000,
-                                                spherical:true
-                                            }
-
-                                        }
-                                        ]);
-
-                    const total = await Product.counDocuments({
-                            genre:{$in:[...categories]},
-                            name:{$regex:data.search,$options:"i"}
-                    });
-                    const response={
-                        error:false,
-                        total,
-                        page:data.page+1,
-                        limit : data.limit,
-                        products
-                    }
-                    return response;
+        const total = geospatialResults[0] || 0;
+        const response={
+            error:false,
+            total:total,
+            page:data.page+1,
+            limit : data.limit,
+            products:geospatialResults
+        }
+        
+        return response;
     }
 }
+
+module.exports = searchService
