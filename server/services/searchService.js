003386658ecf7bfbabb23b7b7771269ffc3c08f8
¨khizzioui¨ <khiziouii@gmail.com>
2024-06-03 12:30:38 +0100

    add location radius in search

--- server/services/searchService.js
+++ server/services/searchService.js
@@ -1,55 +1,61 @@
-require('dotenv').config();
-const Product = require('../Models/product');
-const axios = require('axios');
+require("dotenv").config();
+const Product = require("../Models/product");
+const axios = require("axios");
 
-
-const callback = function(loc){
-    
-    return loc;
-}
 
 
 const searchService = {
-    search : async (data) => {
-        
-        if (!data.location.lat || !data.location.lang){
-            const location = await axios.get("https://ipapi.co/"+data.ip+"/json/");
-            console.log(location.data.latitude);
+  search: async (data) => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let location;
+        if (!data.location.lat || !data.location.lang) {
+          console.log("location not found");
+          const response = await axios.get(`https://ipapi.co/${data.ip}/json/`);
+          location = response.data;
+        } else {
+          location = data.location;
         }
-        
-        console.log(data.ip)
+        //console.log(location);
         const products = await Product.aggregate([
-            {
-                $geoNear: {
-                  near: { type: "Point", coordinates: [
-                    31.6319, -7.998
-                ]},
-                  distanceField: "dist.calculated", // Field to store calculated distance
-                  maxDistance: Number(data.radius) * 1000, // Include maxDistance here
-                  spherical: true
-                }
-              },
-              {
-                $match: {
-                   title: { $regex: data.search, $options: "i" }, 
-                }
+          {
+            $geoNear: {
+              near: { type: "Point", coordinates: [Number(location.latitude),Number(location.longitude)] },
+              distanceField: "dist.calculated",
+              maxDistance: Number(data.radius) * 1000,
+              spherical: true,
+            },
+          },
+          {
+            $match: {
+              title: { $regex: data.search, $options: "i" },
+              price: {
+                $gte: parseInt(data.minprice) || 0,
+                $lte: parseInt(data.maxprice) || Infinity,
               }
+            },
+            
+          },
         ])
-        .skip(data.page*data.limit)
-        .limit(data.limit);
-        
-        const total=products.length
-        const response={
-            error:false,
-            total:total,
-            page:data.page+1,
-            limit : data.limit,
-            products:products,
-            radius:data.radius
-        }
-        
-        return response;
-    }
-}
+          .skip(data.page * data.limit)
+          .limit(data.limit);
+          
+        const total = products.length;
+        const response = {
+          error: false,
+          total,
+          page: data.page + 1,
+          limit: data.limit,
+          products,
+          radius: data.radius,
+        };
 
-module.exports = searchService
+        resolve(response);
+      } catch (error) {
+        reject({ error: true, message: error.message });
+      }
+    });
+  },
+};
+
+module.exports = searchService;
